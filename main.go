b3f7577f76c2fbfe3971572934efc815a5094c7a
Adam Carter <adam@contra-design.com>
2019-09-16 23:57:37 +0100

    Started working on the project

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 )
 
-func handlerFunc (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+var homeTemplate *template.Template
+var contactTemplate *template.Template
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8090", nil)
+	var err error
+
+	homeTemplate, err = template.ParseFiles("views/home.gohtml", "views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml", "views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
+	http.ListenAndServe(":8090", r)
 }
